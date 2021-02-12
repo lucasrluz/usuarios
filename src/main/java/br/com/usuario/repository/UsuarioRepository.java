f6dbf06878d8afba25904eb5e85ad8b3a683ecad
lucasrluz <lucasr.luzbr@gmail.com>
2021-02-11 21:59:32 -0300

    Repository usuario

---
+++ src/main/java/br/com/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.usuario.repository;
+
+import br.com.usuario.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
