310205fe811655f5dfc077fca2ff8ae6aed62e08
lucasrluz <lucasr.luzbr@gmail.com>
2021-02-11 22:54:51 -0300

    Consulta de usuários

--- src/main/java/br/com/usuario/repository/UsuarioRepository.java
+++ src/main/java/br/com/usuario/repository/UsuarioRepository.java
@@ -4,4 +4,7 @@ import br.com.usuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByNome(String nome);
+
 }
